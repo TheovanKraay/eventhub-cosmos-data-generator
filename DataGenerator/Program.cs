cd1bfec163fd800e482f0f4f26c01661a342ce54
TheovanKraay <thvankra@microsoft.com>
2020-03-18 14:52:05 +0000

    Add project files.

---
+++ DataGenerator/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace DataGenerator
+{
+    using Microsoft.Azure.EventHubs;
+    using System;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        private static EventHubClient eventHubClient;
+        private const string EventHubConnectionString = "EventHubConnectionString";
+        private const string EventHubName = "EventHubName";
+
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
+            // Typically, the connection string should have the entity path in it, but for the sake of this simple scenario
+            // we are using the connection string from the namespace.
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
+            {
+                EntityPath = EventHubName
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            //send to the event hubs in parallel
+
+            Thread t1 = new Thread(async () =>
+            {
+                await SendMessagesToEventHub1(1000);
+            });
+
+            Thread t2 = new Thread(async () =>
+            {
+                await SendMessagesToEventHub2(1000);
+            });
+
+            t1.Start();
+            t2.Start();
+
+            //await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        // Uses the event hub client to send 100 messages to the event hub.
+        private static async Task SendMessagesToEventHub1(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    Guid id = Guid.NewGuid();
+                    //var message = $"Message {i}";
+                    var message = "{\"id\":\""+ id + "\",\"pk\": \"uksouth\"}";
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(1000);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+
+        private static async Task SendMessagesToEventHub2(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    Guid id = Guid.NewGuid();
+                    //var message = $"Message {i}";
+                    var message = "{\"id\":\"" + id + "\",\"pk\": \"ukwest\"}";
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(1000);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+    }
+}
---
+++ FunctionRegion2/Function1.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FunctionRegion2
+{
+    public static class Function1
+    {
+
+        private static readonly string _endpointUrl = System.Environment.GetEnvironmentVariable("endpointUrl");
+        private static Uri accountEndPoint = new Uri(_endpointUrl);
+        private static readonly string _primaryKey = System.Environment.GetEnvironmentVariable("primaryKey");
+        private static readonly string _databaseId = "database";
+        private static readonly string _containerId = "asa";
+        private static ConnectionPolicy connectionPolicy = new ConnectionPolicy();
+
+        //req coming from stream analytics would be in something like this format:
+
+        //[{"id":"1","pk": "uksouth"},
+        //{"id":"2","pk": "uksouth"}]
+
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            connectionPolicy.PreferredLocations.Add(LocationNames.UKWest); // first preference
+
+            var messages = JsonConvert.DeserializeObject<List<object>>(requestBody);
+
+            DocumentClient docClient = new DocumentClient(accountEndPoint, _primaryKey, connectionPolicy);
+            foreach (var message in messages)
+            {
+                await docClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(_databaseId, _containerId), message);
+            }
+
+            Console.WriteLine("requestBody: " + requestBody);
+
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            return messages != null
+                ? (ActionResult)new OkObjectResult($"Hello, {messages}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+
+        }
+    }
+}
